fc861bc7ca1a12da017bb497021e01ac0d69f6bb
Helen Dahlquist <quincy.barrettjr@gmail.com>
2022-10-08 18:36:26 -0400

    Pythonnnnn"

--- UIClasses/imageCapture.py
+++ UIClasses/imageCapture.py
@@ -3,10 +3,12 @@ import cv2
 def main():
     camera = cv2.VideoCapture(0)
 
+    print("Capturing")
+
     while True:
         ret, frame = camera.read()
 
-        cv2.imwrite("UIClasses/image.jpg", frame)
+        cv2.imwrite("image.jpg", frame)
 
 if __name__ == "__main__":
     main()
