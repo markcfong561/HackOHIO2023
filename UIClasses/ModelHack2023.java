577abb0fabdd434ed5992239a931f0b5c7333c31
Helen Dahlquist <quincy.barrettjr@gmail.com>
2022-10-08 11:48:19 -0400

    UI class instance variables

---
+++ UIClasses/ModelHack2023.java
@@ -0,0 +1,12 @@
+imoprt java.util.Map; 
+import java.util.HashMap; 
+
+public final class ModelHack2023 {
+
+    private final Map<Pair<Integer, Integer>, Double> people;
+
+    public ModelHack2023() {
+        Map<Pair<Integer, Integer>, Double> people =  new HashMap<Pair<Integer, Integer>, Double>();
+    }
+
+}
