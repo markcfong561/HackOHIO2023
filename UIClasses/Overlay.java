b5791bbbb951f1ded81262ebae2701f53a0550c4
canworm <sarah@keckfamily.org>
2022-10-08 13:12:40 -0400

    testing??

--- UIClasses/Overlay.java
+++ UIClasses/Overlay.java
@@ -1,9 +1,35 @@
-imoprt java.awt.Window; 
-import java.awt.WindowsUtils;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Window;
 
-public class Overlay extends Window {
+class Overlay extends Window {
     public Overlay(Window owner) {
-        super(owner, WindowsUtils.getAlphaCompatibleGraphicsConfiguration()):
+        super(owner, WindowUtils.getAlphaCompatibleGraphicsConfiguration());
         setBackground(new Color(0, 0, 0, 0));
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint gp = new GradientPaint(
+            180.0f, 280.0f,
+            new Color(255, 255, 255, 255),
+            250.0f,
+            380.0f,
+            new Color(255, 255, 0, 0)
+        );
+        g2.setPaint(gp);
+        for (int i = 0; i &lt; 3; i ++ ) {
+            g2.drawOval(150, 280, 100, 100);
+            g2.fillOval(150, 280, 100, 100);
+            g2.translate(120, 20);
+        }
     }
 }
--- UIClasses/ViewHack2023.java
+++ UIClasses/ViewHack2023.java
@@ -1,9 +1,45 @@
-import java.awt.*; 
+import java.awt.*;
 
-public final class ViewHack2023 {
+import javax.swing.JFrame;
 
-    private ControllerHack2023 controller; 
+public final class ViewHack2023 extends JFrame {
 
-    private Overlay overlay; 
+    private ControllerHack2023 controller;
+
+    private Overlay overlay;
+
+    private enum State {
+        PERSON_SEEN, PERSON_LOST
+    }
+
+    // default constructor
+    private State currentState;
+
+    public ViewHack2023() {
+        //call JFrame
+        super("rectangles");
+
+        //set up initial state of GUI as if it has seen nobody
+        this.currentState = State.PERSON_LOST;
+
+        JFrame frame = new JFrame
+
+
+        /*
+         * Make sure the main window is appropriately sized, exits this program
+         * on close, and becomes visible to the user
+         */
+
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    // lets the controller change
+    public void registerObserver(ControllerHack2023 controller) {
+
+        this.controller = controller;
+
+    }
 
 }
